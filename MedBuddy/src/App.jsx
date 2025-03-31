11a60fc7b0355ca69e1a6882d242780d58c247d3
Bhoomi Naik <bhoominaik16@gmail.com>
2025-03-31 22:18:33 +0530

    Added user profile form and logo

--- MedBuddy/src/App.jsx
+++ MedBuddy/src/App.jsx
@@ -4,17 +4,22 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import UserProfile from "./pages/UserProfile"; // Import UserProfile component
 
 const App = () => {
   const [loggedIn, setLoggedIn] = useState(false);
 
   return (
     <>
-      <Navbar loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+      <Navbar loggedIn={loggedIn} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login setLoggedIn={setLoggedIn} />} />
         <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/profile"
+          element={<UserProfile setLoggedIn={setLoggedIn} />}
+        />
       </Routes>
     </>
   );
