5301156a3ec19320d2071bec03472aceb0eececf
Bhoomi Naik <bhoominaik16@gmail.com>
2025-04-01 22:12:05 +0530

    Update Login.jsx

--- MedBuddy/src/pages/Login.jsx
+++ MedBuddy/src/pages/Login.jsx
@@ -15,37 +15,39 @@ const Login = ({ setLoggedIn }) => {
 
   return (
     <div className="flex flex-col items-center justify-center h-[80vh] bg-sky-200">
-      <h2 className="text-2xl font-semibold mb-4">Log In</h2>
-      <form onSubmit={handleSubmit} className="flex flex-col space-y-4 w-72">
-        <input
-          type="email"
-          placeholder="Email"
-          required
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="border p-2 rounded"
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          required
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="border p-2 rounded"
-        />
-        <button type="submit" className="bg-blue-500 text-white py-2 rounded">
-          Login
-        </button>
-      </form>
-      <p className="mt-4 text-gray-600">
-        Don't have an account?{" "}
-        <span
-          className="text-black cursor-pointer font-bold"
-          onClick={() => navigate("/signup")}
-        >
-          Sign Up
-        </span>
-      </p>
+      <div className="border-2 border-solid border-black h-[50vh] w-[30vw] flex flex-col items-center justify-center">
+        <h2 className="text-2xl font-semibold mb-4">Log In</h2>
+        <form onSubmit={handleSubmit} className="flex flex-col space-y-4 w-72">
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="border p-2 rounded"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="border p-2 rounded"
+          />
+          <button type="submit" className="bg-blue-500 text-white py-2 rounded">
+            Login
+          </button>
+        </form>
+        <p className="mt-4 text-gray-600">
+          Don't have an account?{" "}
+          <span
+            className="text-black cursor-pointer font-bold"
+            onClick={() => navigate("/signup")}
+          >
+            Sign Up
+          </span>
+        </p>
+      </div>
     </div>
   );
 };
