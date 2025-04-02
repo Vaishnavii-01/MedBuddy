c01f1a7f52ab9773b91fe478eb2f814b93e1109d
Harshita Singh <harshitaks2005@gmail.com>
2025-04-02 23:03:26 +0530

    added environment variables

--- MedBuddy/email-server/server.js
+++ MedBuddy/email-server/server.js
@@ -84,6 +84,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
 
+
 /*
 import express from "express";
 import nodemailer from "nodemailer";
