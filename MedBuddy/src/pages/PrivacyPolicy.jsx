c3f9dd5a2377c3cc79f4218602ae7e496e989465
Bhoomi Naik <bhoominaik16@gmail.com>
2025-04-06 11:09:53 +0530

    Home page and footer

---
+++ MedBuddy/src/pages/PrivacyPolicy.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PrivacyPolicy = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold">Privacy Policy</h1>
+      <p className="mt-4">Details about our privacy policy go here...</p>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
